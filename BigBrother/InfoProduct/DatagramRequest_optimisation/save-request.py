9542307cbae2c1344274b43641c2cf7c5c4878c0
lucas obadia <52819806+obadial@users.noreply.github.com>
2021-03-11 13:05:08 +0100

    fix security issue

--- BigBrother/InfoProduct/DatagramRequest_optimisation/save-request.py
+++ BigBrother/InfoProduct/DatagramRequest_optimisation/save-request.py
@@ -6,8 +6,8 @@ from pprint import pprint
 
 # 1 STATUT SERVEUR
 
-db_trustbudget = MongoClient("url_mongo_db")
-db_saverequest = db_trustbudget['save-product-data']
+db_trustbudget = MongoClient()
+db_saverequest = db_trustbudget[]
 
 db_champ1 = db_saverequest["product_id"]
 db_champ2 = db_saverequest["date_request"]
