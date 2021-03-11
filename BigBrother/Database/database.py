7943b7170985e0ec2fc8c4fc5f2805af0aca6eba
lucas obadia <52819806+obadial@users.noreply.github.com>
2021-03-11 13:02:27 +0100

    fix security issue

--- BigBrother/Database/database.py
+++ BigBrother/Database/database.py
@@ -7,13 +7,13 @@ class Mydata:
 
 
     def connection_dataBase(self): 
-        client = pymongo.MongoClient('193.70.89.169', 27017, username="lucasO", password="k[NWun695msH")
+        client = pymongo.MongoClient()
         db = client['TrustBudget']
         my_client = db['user-profiles']
         return my_client
 
     def connection_dataBase_item(self):
-        client = pymongo.MongoClient('193.70.89.169', 27017, username="tozziH", password="s3Ep7A5e")
+        client = pymongo.MongoClient()
         db = client['TrustBudget']
         my_client = db['save-product-data']
         return my_client
